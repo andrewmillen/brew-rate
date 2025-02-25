f5e8a70ab9cc4c19509ed71feea3cd8c526d626c
andrewmillen <andrewmillen1@gmail.com>
2025-02-25 15:20:37 -0600

    Update name in UI

--- app/page.js
+++ app/page.js
@@ -28,7 +28,7 @@ export default function Home() {
   return (
     <main className="container mx-auto p-4">
       <div className="flex justify-between items-center mb-4 py-8">
-        <h1 className="text-2xl font-bold">Coffee Bean Journal</h1>
+        <h1 className="text-2xl font-bold">Brew Rate</h1>
         <button
           onClick={() => setIsDrawerOpen(true)}
           className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
