47f97c87a626f1c12f8d99c277fd64ed49234baa
andrewmillen <andrewmillen1@gmail.com>
2025-03-02 15:43:54 -0600

    Update favicon

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>☕️</text></svg>"
+        />
+      </head>
       <body className={`${inter.variable} antialiased bg-gray-100 px-8`}>
         {children}
       </body>
